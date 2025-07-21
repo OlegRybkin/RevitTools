ea29fa972c4a384eada8eb4291fc9f8e8f57b1e6
Oleg Rybkin <@ya.ru>
2025-07-21 08:41:31 +0300

    Изменили классы арматуры, теперь словарь

--- StructConstants.cs
+++ StructConstants.cs
@@ -6,18 +6,17 @@ using System.Threading.Tasks;
 
 namespace RevitTools
 {
-    public enum RebarClass
-    {
-        A240,
-        A500,
-        A500C
-    }
-
     public class StructConstants
     {
         static public List<int> RebarDiameters = new List<int>()
         {
             6, 8, 10, 12, 14, 16, 18, 20, 22, 25, 28, 32, 36, 40
+        };
+
+        static public Dictionary<string, int> RebarClasses = new Dictionary<string, int>
+        {
+            { "А240", 240 },
+            { "А500С", 501 }
         };
 
         

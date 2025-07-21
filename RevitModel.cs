37b44a7317f7d4f4b8af66421a410ed57c0180ce
Oleg Rybkin <@ya.ru>
2025-07-21 17:28:21 +0300

    Добавили копирование параметров

---
+++ RevitModel.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitTools
+{
+    public class RevitModel
+    {
+        /// <summary>
+        /// Копирует параметры из заданного элемента
+        /// </summary>
+        /// <param name="hostElement">Элемент, параметры которого будут копироваться</param>
+        /// <param name="currentElement">Элемент, в который будут записываться параметры</param>
+        /// <param name="groupKR">Параметр "обр_ФОП_Группа КР", который будет записан в элемент</param>
+        static public void CopyParameters(Element hostElement, Element currentElement, string groupKR)
+        {
+            try
+            {
+                currentElement.LookupParameter("ФОП_Блок СМР").Set(hostElement.LookupParameter("ФОП_Блок СМР").AsString());
+                currentElement.LookupParameter("ФОП_Секция СМР").Set(hostElement.LookupParameter("ФОП_Секция СМР").AsString());
+                currentElement.LookupParameter("ФОП_Этаж СМР").Set(hostElement.LookupParameter("ФОП_Этаж СМР").AsString());
+
+                currentElement.LookupParameter("обр_ФОП_Раздел проекта").Set(hostElement.LookupParameter("обр_ФОП_Раздел проекта").AsString());
+                currentElement.LookupParameter("обр_ФОП_Марка ведомости расхода").Set(hostElement.LookupParameter("обр_ФОП_Марка ведомости расхода").AsString());
+                currentElement.LookupParameter("обр_ФОП_Орг. уровень").Set(hostElement.LookupParameter("обр_ФОП_Орг. уровень").AsDouble());
+                currentElement.LookupParameter("обр_ФОП_Группа КР").Set(groupKR);
+            }
+            catch { }
+        }
+    }
+}

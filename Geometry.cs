1f3611b3b9b8da2e8c4d301d9be86eeeb0d55b1e
OlegRybkin <o-rybkin2010@yandex.ru>
2025-07-25 17:32:16 +0300

    добавили RevitModel

---
+++ Geometry.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitTools
+{
+    public class Geometry
+    {
+        static public XYZ GetMidPoint(XYZ point1, XYZ point2)
+        {
+            XYZ midPoint = new XYZ
+                (
+                    0.5 * (point1.X + point2.X),
+                    0.5 * (point1.Y + point2.Y),
+                    0.5 * (point1.Z + point2.Z)
+                );
+
+            return midPoint;
+        }
+
+    }
+}
--- RevitModel.cs
+++ RevitModel.cs
@@ -1,9 +1,15 @@
 ﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace RevitTools
 {
     public class RevitModel
     {
+        static public void RotateAroundVerticalAxis(FamilyInstance familyInstance, XYZ point, double angle)
         /// <summary>
         /// Копирует параметры из заданного элемента
         /// </summary>
@@ -12,18 +18,53 @@ namespace RevitTools
         /// <param name="groupKR">Параметр "обр_ФОП_Группа КР", который будет записан в элемент</param>
         static public void CopyParameters(Element hostElement, Element currentElement, string groupKR)
         {
+            Line axisLine = Line.CreateBound
+                (
+                    point,
+                    new XYZ
+                    (
+                        point.X,
+                        point.Y,
+                        point.Z + 10
+                        )
+                );
             try
             {
                 currentElement.LookupParameter("ФОП_Блок СМР").Set(hostElement.LookupParameter("ФОП_Блок СМР").AsString());
                 currentElement.LookupParameter("ФОП_Секция СМР").Set(hostElement.LookupParameter("ФОП_Секция СМР").AsString());
                 currentElement.LookupParameter("ФОП_Этаж СМР").Set(hostElement.LookupParameter("ФОП_Этаж СМР").AsString());
 
+            familyInstance.Location.Rotate(axisLine, angle);
                 currentElement.LookupParameter("обр_ФОП_Раздел проекта").Set(hostElement.LookupParameter("обр_ФОП_Раздел проекта").AsString());
                 currentElement.LookupParameter("обр_ФОП_Марка ведомости расхода").Set(hostElement.LookupParameter("обр_ФОП_Марка ведомости расхода").AsString());
                 currentElement.LookupParameter("обр_ФОП_Орг. уровень").Set(hostElement.LookupParameter("обр_ФОП_Орг. уровень").AsDouble());
                 currentElement.LookupParameter("обр_ФОП_Группа КР").Set(groupKR);
-            }
+        }
+
+        static public IList<ElementId> MirrorElementsOnPlane(Document document, ICollection<ElementId> elementsToMirror, XYZ point, XYZ direction, bool mirrorCopies)
+        {
+            Plane plane = Plane.CreateByThreePoints
+                (
+                    point,
+                    new XYZ
+                    (
+                        point.X + direction.X * 10,
+                        point.Y + direction.Y * 10,
+                        point.Z
+                        ),
+                    new XYZ
+                    (
+                        point.X,
+                        point.Y,
+                        point.Z + 10
+                        )
+                );
+
+            IList<ElementId> elementIds = ElementTransformUtils.MirrorElements(document, elementsToMirror, plane, mirrorCopies);
+
+            return elementIds;
             catch { }
         }
+
     }
 }
